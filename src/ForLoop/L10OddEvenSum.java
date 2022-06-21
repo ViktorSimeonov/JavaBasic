99e48502499a9a5171a60576b30b155ef228607e
viktor <viktor.simeonov@gmail.com>
2022-06-21 18:41:07 +0300

    java basic

--- src/ForLoop/L10OddEvenSum.java
+++ src/ForLoop/L10OddEvenSum.java
@@ -5,6 +5,22 @@ import java.util.Scanner;
 public class L10OddEvenSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                even += Integer.parseInt(scanner.nextLine());
+            } else {
+                odd += Integer.parseInt(scanner.nextLine());
+            }
 
+
+        }
+        if (even == odd) {
+            System.out.printf("Yes%nSum = %d", even);
+        } else {
+            System.out.printf("No%nDiff = %d%n", Math.abs(even - odd));
+        }
     }
 }
